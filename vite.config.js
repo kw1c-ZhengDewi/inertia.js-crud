54e1295ea98929b47267ce16906d02aa1ee328d5
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-09-19 14:51:33 +0200

    Setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(), //plugin Vue added
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
